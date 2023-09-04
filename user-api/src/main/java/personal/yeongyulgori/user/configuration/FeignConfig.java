cc857c933492f96b8ec3cfb11c741bd27194874f
Sung Hyobin <hellmir610@gmail.com>
2023-09-04 10:01:33 +0900

    ci(configuration): Add FeignConfig.java

---
+++ user-api/src/main/java/personal/yeongyulgori/user/configuration/FeignConfig.java
@@ -0,0 +1,18 @@
+package personal.yeongyulgori.user.configuration;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Qualifier(value = "mailgun")
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("api", "ae55e5bf1eb9c178fe76e06d203f5969-451410ff-b35f5c03");
+    }
+
+}
+
