87cb698d861d6965e9bcd23a8162614358b91855
Sung Hyobin <hellmir610@gmail.com>
2023-09-05 18:24:41 +0900

    test(utility): Add createUser method to reuse test source and role parameter of enterUserForm method in TestObjectFactory class

--- user-api/src/test/java/personal/yeongyulgori/user/testutil/TestObjectFactory.java
+++ user-api/src/test/java/personal/yeongyulgori/user/testutil/TestObjectFactory.java
@@ -1,7 +1,9 @@
 package personal.yeongyulgori.user.testutil;
 
+import personal.yeongyulgori.user.constant.Role;
 import personal.yeongyulgori.user.domain.Address;
 import personal.yeongyulgori.user.domain.SignUpForm;
+import personal.yeongyulgori.user.domain.model.User;
 
 import java.time.LocalDate;
 
@@ -10,7 +12,7 @@ import static org.mockito.Mockito.mock;
 public class TestObjectFactory {
 
     public static SignUpForm enterUserForm(String name, String email, String password,
-                                        LocalDate birthDate, String phoneNumber) {
+                                           LocalDate birthDate, String phoneNumber, Role role) {
 
         return SignUpForm.builder()
                 .name(name)
@@ -19,7 +21,25 @@ public class TestObjectFactory {
                 .birthDate(birthDate)
                 .phoneNumber(phoneNumber)
                 .address(mock(Address.class))
+                .role(role)
                 .profileImage(new byte[]{})
+                .build();
+
+    }
+
+    public static User createUser(String name, String email, String password, LocalDate birthDate,
+                                  String phoneNumber, Role role) {
+
+
+        return User.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .birthDate(birthDate)
+                .phoneNumber(phoneNumber)
+                .address(mock(Address.class))
+                .profileImage(new byte[]{})
+                .role(role)
                 .build();
 
     }
