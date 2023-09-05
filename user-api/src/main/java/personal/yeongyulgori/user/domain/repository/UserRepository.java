02b000f3849923035813a5a020a394f5ac568921
Sung Hyobin <hellmir610@gmail.com>
2023-09-05 18:35:50 +0900

    feat(user): Add methods in UserRepository class
    Add findByEmail and findAllByRole methods in UserRepository class.

--- user-api/src/main/java/personal/yeongyulgori/user/domain/repository/UserRepository.java
+++ user-api/src/main/java/personal/yeongyulgori/user/domain/repository/UserRepository.java
@@ -1,7 +1,16 @@
 package personal.yeongyulgori.user.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import personal.yeongyulgori.user.constant.Role;
 import personal.yeongyulgori.user.domain.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findAllByRole(Role role);
+
 }
