a20240a32bf3b18a363e639d2c818f2b2ec75b43
Sung Hyobin <hellmir610@gmail.com>
2023-09-05 18:29:03 +0900

    test(service): Add role parameter in SignUpServiceTest class
    Change TestPropertySource to ActiveProfiles.

--- user-api/src/test/java/personal/yeongyulgori/user/service/SignUpUserServiceTest.java
+++ user-api/src/test/java/personal/yeongyulgori/user/service/SignUpUserServiceTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
+import personal.yeongyulgori.user.constant.Role;
 import personal.yeongyulgori.user.domain.SignUpForm;
 import personal.yeongyulgori.user.dto.UserResponseDto;
 
@@ -14,7 +15,7 @@ import java.time.LocalDate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static personal.yeongyulgori.user.testutil.TestObjectFactory.enterUserForm;
 
-@TestPropertySource("classpath:application-test.properties")
+@ActiveProfiles("test")
 @SpringBootTest
 class SignUpUserServiceTest {
 
@@ -24,21 +25,23 @@ class SignUpUserServiceTest {
     @DisplayName("사용자가 올바른 양식을 입력하면 회원가입을 할 수 있다.")
     @ParameterizedTest
     @CsvSource({
-            "홍길동, abcd@abc.com, 1234, 2000-01-01, 01012345678",
-            "고길동, abcd@abcd.com, 12345, 2000-02-02, 01012345679",
-            "김길동, abcd@abcde.com, 123456, 2000-03-03, 01012345680",
+            "홍길동, abcd@abc.com, 1234, 2000-01-01, 01012345678, GENERAL_USER",
+            "고길동, abcd@abcd.com, 12345, 2000-02-02, 01012345679, BUSINESS_USER",
+            "김길동, abcd@abcde.com, 123456, 2000-03-03, 01012345680, GENERAL_USER"
     })
     void signUpUser(String name, String email, String password,
-                    LocalDate birthDate, String phoneNumber) {
+                    LocalDate birthDate, String phoneNumber, Role role) {
 
         // given
-        SignUpForm signUpForm = enterUserForm(name, email, password, birthDate, phoneNumber);
+        SignUpForm signUpForm = enterUserForm
+                (name, email, password, birthDate, phoneNumber, role);
 
         // when
         UserResponseDto userResponseDto = signUpUserService.signUpUser(signUpForm);
 
         // then
         assertThat(userResponseDto.getEmail()).isEqualTo(email);
+        assertThat(userResponseDto.getRole()).isEqualTo(role);
 
     }
 
