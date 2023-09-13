d634d5a0947dd55dd6605b3984bcad5ef694def6
Sung Hyobin <hellmir610@gmail.com>
2023-09-14 00:09:13 +0900

    feat(authentication): Add authentication function

---
+++ user-api/src/main/java/personal/yeongyulgori/user/domain/InformationUpdateForm.java
@@ -0,0 +1,30 @@
+package personal.yeongyulgori.user.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+import personal.yeongyulgori.user.constant.Role;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class InformationUpdateForm {
+
+    @ApiModelProperty(value = "id", example = "1")
+    private Long id;
+
+    @ApiModelProperty(value = "성명", example = "홍길동")
+    private String name;
+
+    @ApiModelProperty(value = "주소", example = "2000-01-01")
+    private Address address;
+
+    @ApiModelProperty(value = "분류", example = "GENERAL_USER")
+    private Role role;
+
+    @ApiModelProperty(value = "프로필 이미지 파일")
+    private MultipartFile profileImage;
+
+}
