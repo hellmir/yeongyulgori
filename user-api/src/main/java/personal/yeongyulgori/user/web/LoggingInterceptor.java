faed16a53fd2c4c769fb883ca6cb4dbbc2c97c70
Sung Hyobin <hellmir610@gmail.com>
2023-09-14 00:17:14 +0900

    chore(logging): Add LoggingInterceptor for controllers to log

---
+++ user-api/src/main/java/personal/yeongyulgori/user/web/LoggingInterceptor.java
@@ -0,0 +1,28 @@
+package personal.yeongyulgori.user.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        log.info("Received request for {}", request.getRequestURI());
+
+        return true;
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        log.info("Completed request for {}", request.getRequestURI());
+    }
+
+}
