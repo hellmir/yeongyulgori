30c8e412964f0441bdd8262d29a75524c16d452a
Sung Hyobin <hellmir610@gmail.com>
2023-09-15 20:01:44 +0900

    feat(exception): Add KeywordNotFoundException in AutoCompleteServiceImpl class

--- user-api/src/test/java/personal/yeongyulgori/user/service/AutoCompleteServiceTest.java
+++ user-api/src/test/java/personal/yeongyulgori/user/service/AutoCompleteServiceTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import personal.yeongyulgori.user.exception.serious.sub.KeywordNotFoundException;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -97,6 +99,25 @@ class AutoCompleteServiceTest {
         assertThat(trie.containsKey("홍길동")).isTrue();
         assertThat(trie.containsKey("고길동")).isFalse();
         assertThat(trie.containsKey("김길동")).isTrue();
+
+    }
+
+    @DisplayName("존재하지 않는 키워드를 삭제하려고 하면 KeywordNotFoundException이 발생한다.")
+    @Test
+    void deleteAutoCompleteKeywordByNonExistKeyword() {
+
+        // given
+        String name1 = "홍길순";
+        String name2 = "고길동";
+        String name3 = "김길동";
+
+        autoCompleteService.addAutoCompleteKeyWord(name2);
+        autoCompleteService.addAutoCompleteKeyWord(name3);
+
+        // when, then
+        assertThatThrownBy(() -> autoCompleteService.deleteAutoCompleteKeyword("홍길동"))
+                .isInstanceOf(KeywordNotFoundException.class)
+                .hasMessage("해당 자동완성 키워드가 존재하지 않습니다. keyword: 홍길동");
 
     }
 
