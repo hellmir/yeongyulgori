739baeeb1d1dce93a2fe8614fc9f1ddc56a0025c
Sung Hyobin <hellmir610@gmail.com>
2023-09-18 06:40:17 +0900

    feat(user): Add and change swagger descriptions

--- user-api/src/main/java/personal/yeongyulgori/user/controller/UserController.java
+++ user-api/src/main/java/personal/yeongyulgori/user/controller/UserController.java
@@ -26,31 +26,39 @@ public class UserController {
 
     @ApiOperation(value = "회원 프로필 조회", notes = "다른 회원의 프로필을 조회할 수 있습니다.")
     @GetMapping("{username}")
-    public ResponseEntity<UserResponseDto> getUserProfile(
-            @PathVariable @ApiParam(value = "사용자 이름", example = "gildong1234") String username
-    ) {
+    public ResponseEntity<UserResponseDto> getUserProfile
+            (@PathVariable @ApiParam(value = "사용자 이름", example = "gildong1234") String username) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserProfile(username));
     }
 
-    @ApiOperation(value = "회원 목록", notes = "일부 성명 키워드를 입력해 해당하는 회원 목록을 조회할 수 있습니다.")
+    @ApiOperation(value = "회원 목록",
+            notes = "일부 성명 키워드를 입력해 해당하는 회원 목록을 조회할 수 있습니다. 키워드를 입력하지 않으면 전체 회원 목록을 조회합니다. 페이징 옵션을 선택할 수 있습니다.")
     @ApiImplicitParams({
-            @ApiImplicitParam(
-                    name = "page", value = "현재 페이지 번호", dataType = "int",
-                    paramType = "query", defaultValue = "0"
-            ),
-            @ApiImplicitParam(
-                    name = "size", value = "페이지 당 항목 수", dataType = "int",
-                    paramType = "query", defaultValue = "20"
-            ),
-            @ApiImplicitParam(
-                    name = "sort", value = "정렬 방식 (name,asc, name,desc)",
-                    dataType = "string", paramType = "query"
-            )
+            @ApiImplicitParam(name = "paged", value = "페이지네이션 사용 여부", dataType = "boolean",
+                    paramType = "query", defaultValue = "false", example = "false"),
+            @ApiImplicitParam(name = "unpaged", value = "페이지네이션 미사용 여부", dataType = "boolean",
+                    paramType = "query", defaultValue = "false", example = "false"),
+            @ApiImplicitParam(name = "offset", value = "목록 시작 위치", dataType = "int",
+                    paramType = "query", defaultValue = "0", example = "0"),
+            @ApiImplicitParam(name = "page", value = "현재 페이지 번호", dataType = "int",
+                    paramType = "query", defaultValue = "0", example = "0"),
+            @ApiImplicitParam(name = "pageNumber", value = "현재 페이지 번호", dataType = "int",
+                    paramType = "query", defaultValue = "10", example = "10"),
+            @ApiImplicitParam(name = "size", value = "페이지 당 항목 수", dataType = "int",
+                    paramType = "query", defaultValue = "20", example = "20"),
+            @ApiImplicitParam(name = "pageSize", value = "페이지 당 항목 수", dataType = "int",
+                    paramType = "query", defaultValue = "10", example = "10"),
+            @ApiImplicitParam(name = "sort.sorted", value = "정렬 사용 여부", dataType = "boolean",
+                    paramType = "query", defaultValue = "false", example = "false"),
+            @ApiImplicitParam(name = "sort.unsorted", value = "정렬 미사용 여부", dataType = "boolean",
+                    paramType = "query", defaultValue = "false", example = "false"),
+            @ApiImplicitParam(name = "sort", value = "정렬 방식 (name,asc, name,desc)",
+                    dataType = "string", paramType = "query", example = "name")
     })
     @GetMapping()
-    public ResponseEntity<Page<UserResponseDto>> searchUsers(
-            @ApiParam(value = "성명 키워드", example = "길동") String keyword, Pageable pageable
-    ) {
+    public ResponseEntity<Page<UserResponseDto>> searchUsers
+            (@RequestParam(name = "keyword", required = false, defaultValue = "")
+             @ApiParam(value = "성명 키워드", example = "길동") String keyword, Pageable pageable) {
 
         Page<UserResponseDto> users = userService.getSearchedUsers(keyword, pageable);
 
@@ -58,12 +66,10 @@ public class UserController {
 
     }
 
-    @ApiOperation(
-            value = "키워드로 다른 회원의 성명 검색",
-            notes = "성명의 앞부분 키워드를 입력해 자동 완성 결과를 조회할 수 있습니다."
-    )
+    @ApiOperation(value = "키워드로 다른 회원의 성명 검색",
+            notes = "성명의 앞부분 키워드를 입력해 자동 완성 결과를 조회할 수 있습니다.")
     @GetMapping("/auto-complete")
-    public ResponseEntity<?> autoComplete(@RequestParam @ApiParam(value = "키워드", example = "홍길") String keyword) {
+    public ResponseEntity<?> autoComplete(@ApiParam(value = "키워드", example = "홍길") String keyword) {
 
         List<String> autoCompleteResults = autoCompleteService.autoComplete(keyword);
 
