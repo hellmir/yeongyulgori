2a454f661a1b3d72fc801b152f4a57a76d187146
Sung Hyobin <hellmir610@gmail.com>
2023-09-20 08:46:54 +0900

    feat(post): Add production classes of post service

---
+++ post-api/src/main/java/personal/yeongyulgori/post/domain/post/service/PostService.java
@@ -0,0 +1,22 @@
+package personal.yeongyulgori.post.domain.post.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import personal.yeongyulgori.post.domain.post.model.dto.PostRequestDto;
+import personal.yeongyulgori.post.domain.post.model.dto.PostResponseDto;
+
+public interface PostService {
+
+    PostResponseDto registerPost(Long userId, PostRequestDto postRegisterDto);
+
+    PostResponseDto getPost(Long id, Long writerId);
+
+    Page<PostResponseDto> getAllUserPosts(Long userId, Pageable pageable);
+
+    PostResponseDto updatePost(Long id, Long userId, PostRequestDto postUpdateDto);
+
+    void deletePost(Long id, Long userId);
+
+    Page<PostResponseDto> getSearchedPosts(String keyword, Pageable pageable);
+
+}
