16beca0c59ce5bccec6d6957982ef08d19e1ad0f
Sung Hyobin <hellmir610@gmail.com>
2023-11-10 01:41:29 +0900

    chore(user): Enable spring security

--- user-api/src/main/java/personal/yeongyulgori/user/UserApplication.java
+++ user-api/src/main/java/personal/yeongyulgori/user/UserApplication.java
@@ -1,9 +1,7 @@
 package personal.yeongyulgori.user;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class UserApplication {
 
     public static void main(String[] args) {
