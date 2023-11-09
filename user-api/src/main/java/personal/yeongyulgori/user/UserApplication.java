45feb93cadc393c83168ff2087595863fb33c7f1
Sung Hyobin <hellmir610@gmail.com>
2023-11-10 01:59:08 +0900

    fix(user): Add annotation that evince application for spring boot

--- user-api/src/main/java/personal/yeongyulgori/user/UserApplication.java
+++ user-api/src/main/java/personal/yeongyulgori/user/UserApplication.java
@@ -1,7 +1,9 @@
 package personal.yeongyulgori.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class UserApplication {
 
     public static void main(String[] args) {
