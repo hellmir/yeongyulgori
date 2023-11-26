34ffefa1624d9f04b97e8736ab3e92f8a2a0ab5b
Sung Hyobin <hellmir610@gmail.com>
2023-11-26 23:42:22 +0900

    style(configuration): Add line breaks in SwaggerConfig class

--- user-api/src/main/java/personal/yeongyulgori/user/configuration/SwaggerConfig.java
+++ user-api/src/main/java/personal/yeongyulgori/user/configuration/SwaggerConfig.java
@@ -95,9 +95,13 @@ public class SwaggerConfig {
 
     }
 
-
-    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
-        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+    private boolean shouldRegisterLinksMapping
+            (WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled()
+                && (
+                        StringUtils.hasText(basePath)
+                || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT)
+        );
     }
 
 }
